6b36ba05ffdc704618de3c543083fad141977a97
rishabhgupta <rishabhgupta@msn.com>
2015-06-26 14:58:02 +0530

    Adds migration for response

--- db/migrate/20150626092252_create_responses.rb
+++ db/migrate/20150626092252_create_responses.rb
@@ -1,7 +1,9 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
-
+    	t.references :comment, index: true, foreign_key: true
+    	t.string :commenter
+      t.text :body
       t.timestamps null: false
     end
   end
