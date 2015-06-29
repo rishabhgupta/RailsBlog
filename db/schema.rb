8299b49786973e7f45965b18852ba60c89deb5f8
rishabhgupta <rishabhgupta@msn.com>
2015-06-29 21:01:00 +0530

    Alter articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629101505) do
+ActiveRecord::Schema.define(version: 20150629133012) do
 
   create_table "articles", force: :cascade do |t|
+    t.integer  "blog_id",    limit: 4
     t.string   "title",      limit: 255
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
+  add_index "articles", ["blog_id"], name: "index_articles_on_blog_id", using: :btree
 
   create_table "blogs", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
