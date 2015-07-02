a0f15221e36e1e9884e3457e86a04134a0f07897
rishabhgupta <rishabhgupta@msn.com>
2015-07-02 17:44:57 +0530

    Adds delayed jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629133012) do
+ActiveRecord::Schema.define(version: 20150702080914) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "blog_id",    limit: 4
@@ -43,6 +43,22 @@ ActiveRecord::Schema.define(version: 20150629133012) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   limit: 4,     default: 0, null: false
+    t.integer  "attempts",   limit: 4,     default: 0, null: false
+    t.text     "handler",    limit: 65535,             null: false
+    t.text     "last_error", limit: 65535
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by",  limit: 255
+    t.string   "queue",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "responses", force: :cascade do |t|
     t.integer  "comment_id", limit: 4
